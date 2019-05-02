0d7f4983a2e0ff40bda0033ea369025671e7ad0e
AngOOO <1335208334@qq.com>
2019-05-03 00:47:21 +0800

    Modify Collection

--- JCF/TextCollections/TextMapReduce.java
+++ JCF/TextCollections/TextMapReduce.java
@@ -1,9 +1,10 @@
 package TextCollections;
 
 import java.util.ArrayList;
+import java.util.DoubleSummaryStatistics;
 import java.util.List;
 
-class Order{
+class Order {
     private String title;
     private double price;
     private int amount;
@@ -38,9 +39,76 @@ class Order{
         this.amount = amount;
     }
 }
+
 public class TextMapReduce {
-    public static void TextMapReduce1(){
+    public static void TextMapReduce1() {
         List<Order> orderList = new ArrayList<>();
-        orderList.add()
+        orderList.add(new Order("苹果", 5, 20));
+        orderList.add(new Order("香蕉", 3, 2));
+        orderList.add(new Order("梨", 6.2, 6));
+        orderList.add(new Order("橙子", 3.99, 9));
+        orderList.add(new Order("榴莲", 308.7, 1));
+        orderList.add(new Order("西瓜", 27.8, 1));
+        System.out.println("订单数量：" + orderList.size());
+        System.out.println("订单数量（流）：" + orderList.stream().count());
+
+        double total = 0.0d;
+        for (Order o : orderList) {
+            total += o.getAmount() * o.getPrice();
+        }
+        System.out.println("总价：" + total);
+
+        /*double totalPrice = orderList.stream()
+                .map(order ->
+                {
+                    return order.getPrice() * order.getAmount();
+                })
+                .reduce((sum, item) -> sum = sum + item)
+                .getAsDouble();
+        System.out.println("总价（流）：" + totalPrice);*/
+        double totalPrice = orderList.stream()
+                .mapToDouble(order -> order.getAmount() * order.getPrice())
+                .reduce((sum, item) -> sum = sum + item).getAsDouble();
+        System.out.println("总价（流）：" + totalPrice);
+    }
+
+    public static void TextMapReduce2() {
+        List<Order> orderList = new ArrayList<>();
+        orderList.add(new Order("苹果", 5, 20));
+        orderList.add(new Order("香蕉", 3, 2));
+        orderList.add(new Order("梨", 6.2, 6));
+        orderList.add(new Order("橙子", 3.99, 9));
+        orderList.add(new Order("榴莲", 308.7, 1));
+        orderList.add(new Order("西瓜", 27.8, 1));
+
+        /*double totalPrice = 0.0d;
+        int count = 0;
+        double avgPrice = 0.0d;
+        double maxPrice = Double.MIN_VALUE;
+        double minPrice = Double.MAX_VALUE;
+        for (Order o : orderList) {
+            count++;
+            double price = o.getPrice() * o.getAmount();
+            totalPrice += price;
+            if (price > maxPrice) {
+                maxPrice = price;
+            }
+            if (price < minPrice) {
+                minPrice = price;
+            }
+        }
+        avgPrice = totalPrice / count;*/
+        DoubleSummaryStatistics statistics = orderList.stream()
+                .mapToDouble(order->order.getPrice()*order.getAmount())
+                .summaryStatistics();
+        System.out.println("商品数量：" + statistics.getCount());
+        System.out.println("商品总价：" + statistics.getSum());
+        System.out.println("平均价格：" + statistics.getAverage());
+        System.out.println("订单最大金额：" + statistics.getMax());
+        System.out.println("订单最小金额：" + statistics.getMin());
+    }
+
+    public static void main(String[] args) {
+        TextMapReduce2();
     }
 }
