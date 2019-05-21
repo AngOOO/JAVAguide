1225ace7492891031599f6b163a315ba03d82745
AngOOO <1335208334@qq.com>
2019-05-21 22:13:05 +0800

    stack

--- Stack/src/StackText/Impl/LinkStack.java
+++ Stack/src/StackText/Impl/LinkStack.java
@@ -6,39 +6,63 @@ public class LinkStack<T> implements Stack<T> {
     private class Note {
         private T t;
         private Note next;
+
+        public Note(T t, Note next) {
+            this.t = t;
+            this.next = next;
+        }
+
+        public T getT() {
+            return t;
+        }
     }
 
     private Note head;
 
     @Override
     public boolean push(T t) {
-        Note newNode = new Note();
-        if (newNode.next == null) {
+        Note newNode = new Note(t, null);
+        if (head == null) {
             head = newNode;
-            newNode.t = t;
         } else {
-
+            newNode.next = head.next;
+            head.next = newNode;
         }
-        return false;
+        return true;
     }
 
     @Override
     public T pop() {
-        return null;
+        if (head == null) {
+            return null;
+        } else {
+            head = head.next;
+        }
+        return head.t;
     }
 
     @Override
     public T peek() {
-        return null;
+        if (head == null){
+            return null;
+        }
+        return head.next.t;
     }
 
     @Override
     public int getSize() {
-        return 0;
+        if (head == null){
+            return 0;
+        }
+        int count = 1;
+        for (Note note = head; note != null; note = note.next) {
+            count++;
+        }
+        return count;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return head == null;
     }
 }
--- Stack/src/StackText/Test.java
+++ Stack/src/StackText/Test.java
@@ -1,6 +1,7 @@
 package StackText;
 
 import StackText.Impl.ArrayStack;
+import StackText.Impl.LinkStack;
 
 public class Test {
     public static void main(String[] args) {
@@ -15,5 +16,17 @@ public class Test {
         System.out.println(stack.pop());
         System.out.println(stack.peek());
         System.out.println(stack.getSize());
+        /*LinkStack<Integer> stack = new LinkStack<>();
+        stack.push(999);
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        stack.push(4);
+        stack.push(5);
+        System.out.println(stack.peek());
+        System.out.println(stack.pop());
+        System.out.println(stack.peek());
+        System.out.println(stack.getSize());*/
+        System.out.println(stack.isEmpty());
     }
 }
