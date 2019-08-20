c0b8b1325d8a9edb24526e2f990d621ac1b24e6c
AngOOO <1335208334@qq.com>
2019-08-20 15:42:37 +0800

    webserver

--- webserver_/src/user/LoginServlet.java
+++ webserver_/src/user/LoginServlet.java
@@ -7,16 +7,14 @@ import server.Servlet;
 public class LoginServlet implements Servlet {
     @Override
     public void service(Request req, Response resp) {
-        resp.print("<html>");
-        resp.print("<head>");
-        resp.print("<meta http-equiv=\"content-type\" content=\"text/html;charset=utf-8\">" );
-        resp.print("<title>");
-        resp.print("第一个servlet");
-        resp.print("</title>");
-        resp.print("</head>");
-        resp.print("<body>");
-        resp.print("欢迎回来:"+req.getParaVal("uname"));
-        resp.print("</body>");
-        resp.print("</html>");
+        resp.print("<html>\n" +
+                "\t<head>\n" +
+                "\t\t<meta http-equiv=\"content-type\" content=\"text/html;charset=utf-8\">\t\n" +
+                "\t\t<title>login</title>\n" +
+                "\t</head>\n" +
+                "\t<body>\n" +
+                "\t\t<h1>登陆页面</h1>\t\n" +
+                "\t</body>\n" +
+                "</html>\n");
     }
 }
--- webserver_/src/user/RegisterServlet.java
+++ webserver_/src/user/RegisterServlet.java
@@ -7,28 +7,14 @@ import server.Servlet;
 public class RegisterServlet implements Servlet {
     @Override
     public void service(Request req, Response resp) {
-        String uname = req.getParaVal("uname");
-        String[] favs =req.getParaValues("fav");
-        resp.print("<html>");
-        resp.print("<head>");
-        resp.print("<meta http-equiv=\"content-type\" content=\"text/html;charset=utf-8\">" );
-        resp.print("<title>");
-        resp.print("注册成功");
-        resp.print("</title>");
-        resp.print("</head>");
-        resp.print("<body>");
-        resp.println("你注册的信息为:"+uname);
-        resp.println("你喜欢的类型为:");
-        for(String v:favs) {
-            if(v.equals("0")) {
-                resp.print("萝莉型");
-            }else if(v.equals("1")) {
-                resp.print("豪放型");
-            }else if(v.equals("2")) {
-                resp.print("经济节约型");
-            }
-        }
-        resp.print("</body>");
-        resp.print("</html>");
+        resp.print("<html>\n" +
+                "\t<head>\n" +
+                "\t\t<meta http-equiv=\"content-type\" content=\"text/html;charset=utf-8\">\t\n" +
+                "\t\t<title>register</title>\n" +
+                "\t</head>\n" +
+                "\t<body>\n" +
+                "\t\t<h1>注册页面</h1>\t\n" +
+                "\t</body>\n" +
+                "</html>");
     }
 }
